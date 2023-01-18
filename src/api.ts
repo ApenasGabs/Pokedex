7a62391b1cbcb6075672d3ef6533522721ade02c
Gabriel Rodrigues <Gabers357@gmail.com>
2023-01-18 20:03:09 -0300

    Implementeada lista simples de pokemons #2

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ export const searchPokemon = async (pokemon: string) => {
     console.log("error: ", error);
   }
 };
-export const getPokemon = async (limit = 50, offset = 0) => {
+export const getPokemon = async (limit = 150, offset = 0) => {
   try {
     let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
     const response = await fetch(url);
--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,46 +1,16 @@
 import { Result } from "./Pokedex.types";
-import { Card, List } from "antd";
-
-const data = [
-  {
-    title: "Title 1",
-  },
-  {
-    title: "Title 2",
-  },
-  {
-    title: "Title 3",
-  },
-  {
-    title: "Title 4",
-  },
-];
+import { List } from "antd";
 
 const Pokedex = (props: { pokemons: Result[] | undefined; isLoading: any }) => {
-  const { pokemons, isLoading } = props;
-  console.log("isLoading: ", isLoading);
-  console.log("pokemons: ", pokemons);
-  const pokeList = pokemons?.map((pokemon, idx) => {
-    return (
-      <>
-        <div key={idx}>{pokemon.name}</div>
-        <List
-          grid={{ gutter: 16, column: 4 }}
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <Card title={pokemon.name}>
-                <img src={pokemon.sprites.other["official-artwork"].front_default}
-              </Card>
-            </List.Item>
-          )}
-        />
-      </>
-    );
-  });
-  const noPoke = () => {
-    return <p>Ainda nao ha nada por aqui </p>;
-  };
+  const { pokemons } = props;
+  const pokeList = (
+    <List
+      grid={{ gutter: 16, column: 6 }}
+      dataSource={pokemons}
+      renderItem={(pokemon) => <List.Item>{pokemon.name}</List.Item>}
+    />
+  );
+  const noPoke = <p>Ainda nao ha nada por aqui </p>;
   const resultado = pokemons ? pokeList : noPoke;
   return <>{resultado}</>;
 };
