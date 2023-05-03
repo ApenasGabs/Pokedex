ec86127ef67b5d90cddad7fdd36a8989e4dd1ab4
Gabriel Rodrigues <gabers357@gmail.com>
2023-05-02 22:53:36 -0300

    iniciado criaçao da func getPokelist

--- src/components/Pokemon/PokemonCard.tsx
+++ src/components/Pokemon/PokemonCard.tsx
@@ -1,15 +1,11 @@
-import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Pokemon } from "./PokemonCard.types";
 
-interface Pokemon {
-  name: string;
-  url: string;
-}
 const PokemonCard = (props: { Pokemon: Pokemon }) => {
   const { Pokemon } = props;
   return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import axios from "axios";
 import NavBar from "../components/Navbar";
-import PokemonCard from "../components/Pokemon/PokemonCard";
+import { PokeDex } from "../components/Pokedex/Pokedex";
+import { Pokemon } from "../components/Pokemon/PokemonCard.types";
+import { useState } from "react";
 
-const pokePock = {
+const pokeMock: Pokemon = {
   name: "Poke test",
   url: "https://cdn.dribbble.com/users/1770381/screenshots/5500073/media/0a1c59d162ab651d4881e88bee21b63c.png",
 };
+
 export const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+  for (let i = 0; i > 10; i++) {
+    console.log("baseUrl: ", baseUrl);
+  }
+  const getPokelist = (und = 150) => {
+    const urlLIst = new Array(und)
+      .fill("")
+      .map((_, idx) => `https://pokeapi.co/api/v2/pokemon/${idx}`);
+    console.log("urlLIst: ", urlLIst);
+    axios
+      .all(urlLIst.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res));
+  };
+  getPokelist();
   return (
     <div>
       <NavBar />
-      <PokemonCard Pokemon={pokePock} />
+      <PokeDex Pokemon={pokemons} />
       <div>Made with ❤️ by Apenasgabs</div>
     </div>
   );
